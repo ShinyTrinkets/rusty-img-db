0bce496f0bc1b5174bf10931527b1237a466e896
Cristi Constantin <git@cristi.email>
2023-03-26 23:16:52 +0100

    Added Ripemd hashing

--- src/hashc.rs
+++ src/hashc.rs
@@ -1,6 +1,7 @@
 use blake2::{Blake2b512, Blake2s256};
 use crc32fast;
 use digest::Digest;
+use ripemd::{Ripemd128, Ripemd160};
 use sha2::{Sha224, Sha256, Sha384, Sha512};
 use std::fmt;
 use std::hash::Hasher;
@@ -9,13 +10,15 @@ use strum_macros::EnumString;
 /// Cryptographical hash
 pub fn hash_c(h: &HashC, data: &[u8]) -> String {
     match h {
+        HashC::Crc32 => crc_hex::<crc32fast::Hasher>(data),
+        HashC::Blake256 => hash_hex::<Blake2s256>(data),
+        HashC::Blake512 => hash_hex::<Blake2b512>(data),
+        HashC::Ripemd128 => hash_hex::<Ripemd128>(data),
+        HashC::Ripemd160 => hash_hex::<Ripemd160>(data),
         HashC::Sha224 => hash_hex::<Sha224>(data),
         HashC::Sha256 => hash_hex::<Sha256>(data),
         HashC::Sha384 => hash_hex::<Sha384>(data),
         HashC::Sha512 => hash_hex::<Sha512>(data),
-        HashC::Blake256 => hash_hex::<Blake2s256>(data),
-        HashC::Blake512 => hash_hex::<Blake2b512>(data),
-        HashC::Crc32 => crc_hex::<crc32fast::Hasher>(data),
     }
 }
 
@@ -36,12 +39,14 @@ fn crc_hex<T: Hasher>(data: &[u8]) -> String {
 #[strum(ascii_case_insensitive)]
 pub enum HashC {
     Crc32,
+    Blake256,
+    Blake512,
+    Ripemd128,
+    Ripemd160,
     Sha224,
     Sha256,
     Sha384,
     Sha512,
-    Blake256,
-    Blake512,
 }
 
 impl fmt::Display for HashC {
